5bc1e28621a8a6bf6299755f622fb3fda3746518
ki <kirillbanhenco@gmail.com>
2025-01-21 13:26:31 +0300

    added placeholders

--- front/src/components/Filter.jsx
+++ front/src/components/Filter.jsx
@@ -13,12 +13,12 @@ const Filter = () => {
                 <p>Наша цель — сделать ваши приключения незабываемыми, предлагая широкий спектр услуг, которые помогут вам открыть новые горизонты и насладиться каждой минутой вашего путешествия.</p>
 
                 <form>
-                    <input id='from' type="text" />
-                    <input id='where' type="text" />
-                    <input id='dateLeaving' type="date" />
-                    <input id='nights' type="number" />
-                    <input id='countAdults' type="number" />
-                    <input id='countChildren' type="number" />
+                    <input id='from' type="text" placeholder='Откуда' />
+                    <input id='where' type="text" placeholder='Куда' />
+                    <input id='dateLeaving' type="date" value={'Дата вылета'}/>
+                    <input id='nights' type="number" placeholder='Ночей' />
+                    <input id='countAdults' type="number" placeholder='Кол-во взрослых' />
+                    <input id='countChildren' type="number" placeholder='Кол-во детей' />
 
                     <button className={styles.buttonFind}>Найти</button>
                 </form>
