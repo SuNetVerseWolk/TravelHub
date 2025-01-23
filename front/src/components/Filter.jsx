448438b518c01372de0754124dc4e2f030b272ae
ki <kirillbanhenco@gmail.com>
2025-01-23 13:08:12 +0300

    changed filter

--- front/src/components/Filter.jsx
+++ front/src/components/Filter.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, use, useState } from 'react'
 import styles from 'styles/filter.module.css'
 
 const Filter = () => {
+  // const startDateInput = use();
+  // const endDateInput = use();
+  const [startDateInput, setStartDateInput] = useState();
+  const [endDateInput, setEndDateInput] = useState();
+
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  useEffect(() => {
+    setStartDateInput(document.getElementById('startDateInput'));
+    setEndDateInput(document.getElementById('endDateInput'));
+  }, []);
+  
+  const convertToShortDate = (date) => date ? new Date(date).toLocaleString('ru', {month: 'short', day: 'numeric'}) : '';
+
+  const getDateFrom2day = (number = 0) => {
+    let today = new Date();
+    today.setDate(new Date().getDate() + number);
+    return today.toLocaleString('ru', {month: 'short', day: 'numeric'});
+  }
+
   return (
     <>
         <div className={styles.filterContainer}>
@@ -15,8 +36,19 @@ const Filter = () => {
                 <form>
                     <input id='from' type="text" placeholder='Откуда' />
                     <input id='where' type="text" placeholder='Куда' />
-                    <input id='dateLeaving' type="date" value={'Дата вылета'}/>
-                    <input id='nights' type="number" placeholder='Ночей' />
+
+                    <div className={styles.dates}>
+                      <button type='button' onClick={e => startDateInput.showPicker()}>
+                        С <span>{startDateInput?.value ? convertToShortDate(startDateInput?.value) : getDateFrom2day() }</span>
+                        <input id='startDateInput' type='date' onChange={(e) => setStartDate(e.target.value)} style={{width: 0, padding: 0, margin: 0}} />
+                      </button>
+                        /
+                      <button type='button' onClick={e => endDateInput.showPicker()}>
+                        По <span>{endDateInput?.value ? convertToShortDate(endDateInput?.value) : getDateFrom2day(1) }</span>
+                        <input id='endDateInput' type='date' onChange={(e) => setEndDate(e.target.value)} style={{width: 0, padding: 0, margin: 0}} />
+                      </button>
+                    </div>
+
                     <input id='countAdults' type="number" placeholder='Кол-во взрослых' />
                     <input id='countChildren' type="number" placeholder='Кол-во детей' />
 
