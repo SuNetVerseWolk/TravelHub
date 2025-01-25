523b212bd194ed24fd3d6417389796b9bfdeac4d
SuNetVerseWolk <volk333333333@gmail.com>
2025-01-25 20:08:26 +0300

    demo deploy with back

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const backPath = process.env.VITE_BACK;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +14,8 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3002',
+        //target: 'http://localhost:3002',
+				target: backPath,
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
