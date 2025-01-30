a53544eb2514a73c16e15440d175484eb1aac67d
SuNetVerseWolk <volk333333333@gmail.com>
2025-01-30 14:26:35 +0300

    users
    changed style and added number formater; added font sizes into root css

--- front/src/layouts/Users.jsx
+++ front/src/layouts/Users.jsx
@@ -2,6 +2,7 @@ import { getUsers } from "api/get";
 import React from "react";
 import styles from "styles/users.module.css";
 import { motion } from "framer-motion";
+import formatPhoneNumberForDisplay from "api/formatPhoneNumber";
 
 export const Users = ({ extraStyles }) => {
   const { data: users, refetch } = getUsers();
@@ -10,11 +11,13 @@ export const Users = ({ extraStyles }) => {
     <div className={`${extraStyles} ${styles.main}`}>
       {users?.map((user) => (
         <div key={user.id} className={styles.item}>
-          <h2>{user.name}</h2>
-          <p>{user.lastName}</p>
-          <p>{user.fatherName}</p>
+					<div className={styles.title}>
+						<h2>{user.lastName}</h2>
+						<p>{user.name}</p>
+						<p>{user.fatherName}</p>
+					</div>
           <p>{user.email}</p>
-          <p>{user.number}</p>
+          <p>{formatPhoneNumberForDisplay(user.number)}</p>
           <motion.button
             whileHover={{ background: "#000", color: "#fff" }}
             initial={{ background: "#fff", color: "#000" }}
